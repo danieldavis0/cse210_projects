307645b7d251fd6728090ef8223bc78960874e94
Daniel <garland@Garlands.PC>
2025-03-12 09:20:16 -0700

    Added Jobs class and code to use it for the resumes activity.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "Amazon";
+        job1._jobTitle = "Warehouse Worker";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+
+        job2._company = "Microsoft";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+
+        job1.Display();
+        job2.Display();
     }
 }
