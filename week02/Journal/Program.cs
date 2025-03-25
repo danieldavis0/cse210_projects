2767b6ea86088273b0621539dc050396bacb270d
Daniel <garland@Garlands.PC>
2025-03-25 12:56:41 -0700

    Completed main Journal program and associated classes (Journal, Entry, and PromptGenerator).

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,14 +1,75 @@
+// For my creative additions I have added logic to the PromptGenerator class to ensure the same prompt will not show
+// up for the user twice in a row. This is done by saving the last used prompt to variable, which will be checked against
+// the next prompt generated.
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Boolean runLoop = true;
+        bool runLoop = true;
+        string userInput = "";
+        string mostRecentPrompt = "";
+        Journal journal = new Journal();
+        
+        Console.WriteLine("Welcome to the Journal Program!");
+
         while (runLoop)
         {
             Console.WriteLine("Please select one of the following options: \n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
-            Console.WriteLine("What is your guess? ");
+            Console.WriteLine("What would you like to do? ");
+
+            while (userInput != "1" && userInput != "2" && userInput != "3" &&userInput != "4" && userInput != "5")
+            {
+                userInput = Console.ReadLine();
+                if (userInput != "1" && userInput != "2" && userInput != "3" &&userInput != "4" && userInput != "5")
+                {
+                    Console.WriteLine("Please enter a valid input.");
+                }
+            }
+
+            if (userInput == "1")
+            {
+                Entry entry = new Entry();
+                PromptGenerator prompt = new PromptGenerator();
+                string newPrompt = prompt.GetRandomPrompt(mostRecentPrompt);
+                mostRecentPrompt = newPrompt;
+                Console.WriteLine(newPrompt);
+                userInput = Console.ReadLine();
+                entry._promptText = newPrompt;
+                entry._entryText = userInput;
+                DateTime currentTime = DateTime.Now;
+                string timeStamp = currentTime.ToShortDateString();
+                entry._date = timeStamp;
+                journal.AddEntry(entry);
+            }
+
+            else if (userInput == "2")
+            {
+                journal.DisplayAll();
+            }
+
+            else if (userInput == "3")
+            {
+                Console.WriteLine("What is the filename?");
+                userInput = Console.ReadLine();
+                journal.LoadFromFile(userInput);
+            }
+
+            else if (userInput == "4")
+            {
+                Console.WriteLine("What is the filename?");
+                userInput = Console.ReadLine();
+                journal.SaveToFile(userInput);
+            }
+
+            else if (userInput == "5")
+            {
+                runLoop = false;
+            }
+
+            userInput = "";
         }
     }
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,10 +1,29 @@
 public class PromptGenerator
 {
-    public List<string> _entries = new List<string>();
+    
+    public List<string> _prompts = new List<string>(["Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?", "Whose life did I positively affect today?", "How satisfied am I with how my day went?"]);
 
-    public string GetRandomPrompt()
+    public string GetRandomPrompt(string mostRecentPrompt)
     {
-        return "";
+        Random random = new Random();
+        int lengthOfList = _prompts.Count();
+        long randomNumber = random.NextInt64(lengthOfList);
+        int promptNumber = Convert.ToInt32(randomNumber);
+        string newPrompt = _prompts[promptNumber];
+
+        if (newPrompt == mostRecentPrompt)
+        {
+            while (newPrompt == mostRecentPrompt)
+            {
+                randomNumber = random.NextInt64(lengthOfList);
+                promptNumber = Convert.ToInt32(randomNumber);
+                newPrompt = _prompts[promptNumber];
+            }
+        }
+
+        mostRecentPrompt = newPrompt;
+
+        return newPrompt;
     }
 }
 
